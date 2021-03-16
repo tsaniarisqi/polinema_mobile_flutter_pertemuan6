84e13b01b6540e4acd055c43c60d86eb6c61744a
tsaniarisqi <tsaniarisqi98@gmail.com>
2021-03-16 19:22:06 +0700

    Mendefinisika Route

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,16 @@
-void main() {}
+import 'dart:js';
+
+import 'package:belanja/pages/home_pages.dart';
+import 'package:belanja/pages/item_pages.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => HomePage(),
+      '/item': (context) => ItemPage(),
+    },
+  ));
+}
